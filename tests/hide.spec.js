c25698b0fdf60df4e92a2361ec8033dfeeaa99fd
Greg Friedman <greg@upallnightconsulting.com>
2026-09-01 11:37:29 -0700

    add test coverage before migrating to safari

--- tests/hide.spec.js
+++ tests/hide.spec.js
@@ -7,6 +7,21 @@ import { gotoFixture } from '../utils/testUtils/gotoFixture.js'
 const __dirname = dirname(fileURLToPath(import.meta.url))
 const fixturesDir = join(__dirname, './fixtures')
 
+// mask elements so we don't violate the Terms of Service by including YT owned content
+//  in the screenshots
+const complianceMasks = page => [
+  page.locator('#logo'),
+  page.locator('#description-inline-expander'),
+  page.locator('#avatar'),
+  page.locator('#channel-name'),
+  page.locator('#title'),
+  page.locator('svg'),
+  page.locator('img'),
+  page.locator('yt-lockup-metadata-view-model'),
+  page.getByText(/youtube/i),
+  page.getByText(/google/i),
+]
+
 describe('homepage', () => {
   test.use({ viewport: { width: 1600, height: 1200 } })
   test('only the masthead with the search bar is visible', async ({ page }) => {
@@ -19,8 +34,33 @@ describe('homepage', () => {
     // Assert
     await expect(page.locator('body')).toHaveClass(/hideMode/) // main.js ran
     await expect(page).toHaveScreenshot('homepage-hidden.png', {
-      // mask the logo so we don't violate the Terms of Service by including YT owned content
-      mask: [page.locator('#logo')],
+      mask: complianceMasks(page),
+    })
+  })
+
+  test('turning off YouFocus in popup shows complete homepage', async ({
+    page,
+    extensionId,
+  }) => {
+    // Arrange
+    const fixture = join(fixturesDir, 'home/index.html')
+
+    await page.goto(`chrome-extension://${extensionId}/popup.html`)
+
+    // Act
+    const youFocusToggle = page.locator('header button')
+    await youFocusToggle.click()
+    // make sure the toggle is now disabled
+    // this also ensures the updated value has been written asynchronously to
+    //  chrome storage before switching to the fixture page
+    await expect(youFocusToggle).toHaveClass(/toggle-off/)
+
+    await gotoFixture(page, fixture)
+
+    // Assert
+    await expect(page.locator('body')).not.toHaveClass(/hideMode/) // main.js ran
+    await expect(page).toHaveScreenshot('homepage-shown.png', {
+      mask: complianceMasks(page),
     })
   })
 })
@@ -37,14 +77,25 @@ describe('watch', () => {
     // Assert
     await expect(page.locator('body')).toHaveClass(/hideMode/) // main.js ran
     await expect(page).toHaveScreenshot('watch-hidden.png', {
-      // mask the logo so we don't violate the Terms of Service by including YT owned content
-      mask: [
-        page.locator('#logo'),
-        page.locator('#description-inline-expander'),
-        page.locator('#avatar'),
-        page.locator('#channel-name'),
-        page.locator('#title'),
-      ],
+      mask: complianceMasks(page),
+    })
+  })
+  test('turning off YouFocus in popup shows complete watch page', async ({
+    page,
+    extensionId,
+  }) => {
+    // Arrange
+    const fixture = join(fixturesDir, 'watch/index.html')
+    await page.goto(`chrome-extension://${extensionId}/popup.html`)
+
+    // Act
+    await page.locator('header button').click()
+    await gotoFixture(page, fixture)
+    // await page.pause()
+    // Assert
+    await expect(page.locator('body')).not.toHaveClass(/hideMode/) // main.js ran
+    await expect(page).toHaveScreenshot('watch-shown.png', {
+      mask: complianceMasks(page),
     })
   })
 })
@@ -61,8 +112,7 @@ describe('shorts', () => {
     // Assert
     await expect(page.locator('body')).toHaveClass(/hideMode/) // main.js ran
     await expect(page).toHaveScreenshot('shorts-hidden.png', {
-      // mask the logo so we don't violate the Terms of Service by including YT owned content
-      mask: [page.locator('#logo')],
+      mask: complianceMasks(page),
     })
   })
 })
--- utils/testUtils/testWithExtension.js
+++ utils/testUtils/testWithExtension.js
@@ -4,6 +4,7 @@ import { fileURLToPath } from 'node:url'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
+// @see https://playwright.dev/docs/chrome-extensions#testing
 export const test = base.extend({
   context: async ({ viewport }, use) => {
     const pathToExtension = join(__dirname, '../../dist')
@@ -22,6 +23,17 @@ export const test = base.extend({
     })
     await use(context)
     await context.close()
+  },
+  // We don't have a service worker so we can't use Playwright's proposed extensionId
+  //  fixture code. So instead, we open the extensions-internals page which lists every
+  //  extension and extract the YouFocus id and then make that available to the tests
+  //  e.g. `test('do something', { page, extensionId } => {...})`
+  extensionId: async ({ context }, use) => {
+    const page = await context.newPage()
+    await page.goto(`chrome://extensions-internals`)
+    const internals = JSON.parse(await page.locator('body').innerText())
+    const extensionId = internals.find(e => e.name === 'YouFocus')?.id
+    await use(extensionId)
   },
 })
 
