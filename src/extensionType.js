7350f53fb131bde132a10a543c878fe7036ffbf0
Greg Friedman <greg@upallnightconsulting.com>
2026-09-02 14:01:35 -0700

    cleanup

--- src/extensionType.js
+++ src/extensionType.js
@@ -1,5 +1,11 @@
 import { browser } from './browser.js'
 
+/**
+ * Extract the extension type from the provided url presuming that
+ *  it is following the standard browser extension scheme like 'chrome-extension://'
+ * @param {string} url - url from browser.runtime.getURL
+ * @return {string}
+ */
 export function extensionTypeFromUrl(url) {
   const [, extensionType] = /^(.*?)-.*extension:\/\//.exec(url) || []
   return extensionType
